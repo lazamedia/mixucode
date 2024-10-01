d2a604d3b918fb1a49fcc7c34724b879d8f7ce47
lazuardi <lazamediamxt@gmail.com>
2024-10-01 10:40:20 +0700

    first commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -28,7 +28,7 @@
     <link rel="stylesheet" href="{{ asset('css/layoutsmain.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/js/bootstrap.min.js') }}">
-    <link rel="stylesheet" href="{{ asset ('css/home.css') }}">
+    <link rel="stylesheet" href="{{ asset ('css/homenew.css') }}">
     
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
